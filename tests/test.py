33ed4a1c191be16638bb1427918ce53812d04eb3
YazanW1 <165731598+YazanW1@users.noreply.github.com>
2025-05-21 04:35:50 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,12 @@
 import subprocess
+import os
 
 def test_hello_output():
+    # Get the absolute path to Hello.py (parent folder)
+    script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Hello.py"))
+
     result = subprocess.run(
-        ["python", "Hello.py"],
+        ["python", script_path],
         capture_output=True,
         text=True
     )
